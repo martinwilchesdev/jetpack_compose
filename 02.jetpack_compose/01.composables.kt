ddec8e1c61a90b91cd339f94620d04c03307a074
Martin Wilches <martindotdev@gmail.com>
2024-02-12 22:26:52 -0500

    Composables y modificadores

---
+++ 02.jetpack_compose/01.composables.kt
@@ -0,0 +1,46 @@
+// MainActivity.kt
+
+// El package hace referencia al nombre de la aplicacion y a su identificador principal
+package com.example.appname
+
+// Toda aplicacion debe tener la siguiente clase en su archivo principal
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Dentro del setContent se define todo lo que se visualiza en la aplicacion
+        setContent {
+            Greeting("Android")
+        }
+    }
+}
+
+/*
+    Toda funcion que lleve la palabra Composable, convierte lo que tenga dentro de esta en un componente.
+    Todas las funciones que sean Composable deberian iniciar con mayuscula.
+*/
+@Composable
+fun Greeting(name: String) {
+    Button(onClick = {
+        println("Hello World")
+    }) {
+        Text(text = "Hello $name")
+    }
+}
+
+/*
+    Las funciones que no san Composable pueden ejecutar cualquier tipo de logica dentro de la aplicacion, por ejemplo:
+
+        - Una consulta a una base de datos.
+        - Una peticion a un servicio externo.
+*/
+fun hello() {
+    println("Hello Jetpack Compose")
+}
+
+// La previsaulizacion se utiliza para visualizar el diseño de la aplicacion sin necesidad de ejecutar el emulador o la aplicacion desde un dispositivo
+@Preview(showBackground=true)
+@Composable
+fun GreetingPreview() {
+    Greeting("Android Preview")
+}
---
+++ 02.jetpack_compose/02.modifiers.kt
@@ -0,0 +1,34 @@
+class MainActivity : ComponentActivity() {
+    /**/
+}
+
+// Los modificadores ayudan a personalizar la apariencia y el comportamiento de los componentes
+@Preview(showBackground = true)
+@Composable
+fun Content() {
+    Text(
+        text = "Hello Jetpack Compose",
+        modifier = Modifier
+            .background(Color.Black)
+            .padding(50.dp)
+            .clickable {
+                println("Hello Jetpack Compose")
+            }
+    )
+}
+
+/*
+    Existen 4 tipos de modificadores
+
+        - De posicionamiento = width, height
+        - De funcionalidad = click, scroll
+        - De apariencia = background, padding, border
+        - De escucha = onKeyEvent
+
+    El orden en que son encadenados los modificares en un componente es importante
+
+        .padding(50.dp)
+        .background(Color.Black)
+
+    ** En el ejemplo anterior primero se aplica el padding y luego el color de fondo, por lo cual el espacio que ocupe el padding no tendra el color de fondo especificado
+*/
